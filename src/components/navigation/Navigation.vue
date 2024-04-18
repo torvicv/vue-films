15e73ef9eff2f5a52248e1344e841b3b298d15cc
victor <torvicv@gmail.com>
2024-04-18 22:06:50 +0200

    mejores peliculas

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,10 +1,11 @@
 <template>
-    <p>
+  <p>
     <!-- use router-link component for navigation. -->
     <!-- specify the link by passing the `to` prop. -->
     <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
     <router-link to="/cartelera">Cartelera</router-link>
     <router-link to="/popular">Populares</router-link>
     <router-link to="/proximas">Proximamente</router-link>
+    <router-link to="/mejores">Mejores películas</router-link>
   </p>
 </template>
--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import { createWebHashHistory, createRouter } from "vue-router";
 import Playing from "./components/pages/Playing.vue";
 import Popular from "./components/pages/Popular.vue";
 import Upcoming from "./components/pages/Upcoming.vue";
+import Top from "./components/pages/Top.vue";
 
 
 const routes = [
     { path: '/cartelera', component: Playing},
     { path: '/popular', component: Popular},
     { path: '/proximas', component: Upcoming},
+    { path: '/mejores', component: Top},
 ];
 
 const router = createRouter({history: createWebHashHistory(), routes});
